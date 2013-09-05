f44819f785283a961fe703bb539a8077d9623176
mlerner <mlerner789@gmail.com>
2013-09-04 22:07:10 -0400

    Adding rgeo factory

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
+  self.rgeo_factory_generator = RGeo::Geos.factory_generator(srid: 4326)
+
   attr_accessible :description, :status, :title
   belongs_to :user
   has_one :assignee
