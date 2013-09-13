4442e7a100a5a4c8eff19d29dfaf50010ab03de0
mlerner <mlerner789@gmail.com>
2013-09-12 23:20:58 -0400

    Initial commit of search page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def index
+    @recent_tasks = Task.last(5)
     render template: 'pages/index'
+  end
+
+  def browse
+    render template: 'pages/browse'
+  end
+
+  def search
+    render template: 'pages/search'
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,25 @@
 module PagesHelper
+  def current_tab
+    params[:tab] ||= 'helpers'
+  end
+
+  def current_sort_order
+    params[:sort] ||= 'top'
+  end
+
+  def current_time_order
+    params[:time] ||= 'today'
+  end
+
+  def generate_class(tab, other_styles = nil)
+    tab << ' selected' if tab_selected?(tab)
+    tab
+
+  end
+
+  def tab_selected?(tab)
+    tab == current_tab ||
+    tab == current_sort_order ||
+    tab == current_time_order
+  end
 end
