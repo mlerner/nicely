a84b83c4c6fa1fa4c95d9f54bf2a3ddd2890b111
Micah Lerner <mlerner789@gmail.com>
2014-02-16 22:06:24 -0800

    Fixing db columns

--- db/migrate/20130902214831_create_tasks.rb
+++ db/migrate/20130902214831_create_tasks.rb
@@ -4,6 +4,8 @@ class CreateTasks < ActiveRecord::Migration
       t.string :title
       t.integer :status
       t.string :description
+      t.point :start_xy, :geographic => true
+      t.point :end_xy, :geographic => true
 
       t.timestamps
     end
--- db/migrate/20130905013043_add_points_to_task.rb
+++
@@ -1,6 +0,0 @@
-class AddPointsToTask < ActiveRecord::Migration
-  def change
-    add_column :tasks, :start_xy, :geometry, :srid => 4326 
-    add_column :tasks, :end_xy, :geometry, :srid => 4326 
-  end
-end
