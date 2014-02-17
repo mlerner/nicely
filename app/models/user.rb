3ae725f5336805ff2d096bef19d83ead278f051a
Micah Lerner <mlerner789@gmail.com>
2014-02-16 18:49:37 -0800

    Add default name

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
+      user.name = auth.info.email
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.save!
